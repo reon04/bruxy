400fefd27f151ab6516c75450fa53720766d917a
reon04 <larsmichels04@gmail.com>
2024-10-31 15:37:24 +0100

    fix bug where port is included in nginx server name

--- src/startup.sh
+++ src/startup.sh
@@ -6,6 +6,9 @@ long=${LONG_DOMAIN:-ruhr-uni-bochum.de}
 shortr=$(echo "$short" | sed "s/\./\\\\\\\\./g")
 longr=$(echo "$long" | sed "s/\./\\\\\\\\./g")
 
+shortr=$(echo "$shortr" | sed "s/:[0-9]*//g")
+longr=$(echo "$longr" | sed "s/:[0-9]*//g")
+
 cp $SCRIPTPATH/nginx.conf.template $SCRIPTPATH/nginx.conf
 
 sed -i -e "s/?replace_label_short_domain?/$short/g" $SCRIPTPATH/nginx.conf
